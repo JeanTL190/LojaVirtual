7435005ac8b246bdb10a4d01376ef33519cfb2fc
Jean Tan Li <jeantl@outlook.com.br>
2021-03-24 17:11:19 -0300

    aula 133 feita

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -6,10 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Estrela Oriental',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          primaryColor: Color.fromARGB(255, 4, 125, 141)),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
