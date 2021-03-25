1ef172237976246fa0bbd112a37d524a4c093fb4
Jean Tan Li <jeantl@outlook.com.br>
2021-03-25 15:57:59 -0300

    Aula 139 feita

--- loja_virtual/lib/tiles/drawer_tile.dart
+++ loja_virtual/lib/tiles/drawer_tile.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 class DrawerTile extends StatelessWidget {
   final IconData icon;
   final String text;
-  DrawerTile(this.icon, this.text);
+  final PageController controller;
+  final int page;
+  DrawerTile(this.icon, this.text, this.controller, this.page);
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).pop();
+          controller.jumpToPage(page);
+        },
         child: Container(
           height: 60,
           child: Row(
@@ -17,14 +22,21 @@ class DrawerTile extends StatelessWidget {
               Icon(
                 icon,
                 size: 32,
-                color: Colors.black,
+                color: controller.page.round() == page
+                    ? Theme.of(context).primaryColor
+                    : Colors.grey[700],
               ),
               SizedBox(
                 width: 32,
               ),
               Text(
                 text,
-                style: TextStyle(fontSize: 16, color: Colors.black),
+                style: TextStyle(
+                  fontSize: 16,
+                  color: controller.page.round() == page
+                      ? Theme.of(context).primaryColor
+                      : Colors.grey[700],
+                ),
               )
             ],
           ),
