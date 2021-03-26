0e2b44d6770d9786af72b18e3089ae1eaee52022
Jean Tan Li <jeantl@outlook.com.br>
2021-03-26 18:15:23 -0300

    Aula 148 feita!

--- loja_virtual/lib/screens/product_screen.dart
+++ loja_virtual/lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
@@ -15,6 +16,50 @@ class _ProductScreenState extends State<ProductScreen> {
   _ProductScreenState(this.product);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme.of(context).primaryColor;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 4,
+              dotSpacing: 15,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              autoplay: true,
+              autoplayDuration: Duration(seconds: 7),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
