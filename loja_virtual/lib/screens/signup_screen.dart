6eb7fd3dfd462916fff5c5ac1d9c459d69d6d7ad
Jean Tan Li <jeantl@outlook.com.br>
2021-03-27 16:46:43 -0300

    Aula 155 feita!

--- loja_virtual/lib/screens/signup_screen.dart
+++ loja_virtual/lib/screens/signup_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class SignUpScreen extends StatelessWidget {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _addressController = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -9,70 +16,95 @@ class SignUpScreen extends StatelessWidget {
         title: Text("Criar Conta"),
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: "Nome Completo"),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty) return "Nome inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "E-mail"),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty || !text.contains("@"))
-                  return "E-mail inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Senha"),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) return "Senha inválida!";
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Endereço"),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty) return "Endereço inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              height: 44,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
-                child: Text(
-                  "Cadastrar",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(child: CircularProgressIndicator());
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(hintText: "Nome Completo"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty) return "Nome inválido!";
+                  },
                 ),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith(
-                        (states) => Theme.of(context).primaryColor)),
-              ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(hintText: "E-mail"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty || !text.contains("@"))
+                      return "E-mail inválido!";
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: _passController,
+                  decoration: InputDecoration(hintText: "Senha"),
+                  obscureText: true,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6)
+                      return "Senha inválida!";
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: _addressController,
+                  decoration: InputDecoration(hintText: "Endereço"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty) return "Endereço inválido!";
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Map<String, dynamic> userData = {
+                          "name": _nameController.text,
+                          "email": _emailController.text,
+                          "address": _addressController.text,
+                        };
+                        model.signUp(
+                            userData: userData,
+                            pass: _passController.text,
+                            onSucess: _onSucess,
+                            onFail: _onFail);
+                      }
+                    },
+                    child: Text(
+                      "Cadastrar",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.resolveWith(
+                            (states) => Theme.of(context).primaryColor)),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
+
+  void _onSucess() {}
+  void _onFail() {}
 }
