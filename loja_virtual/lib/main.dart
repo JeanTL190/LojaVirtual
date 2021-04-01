c71caab4a3d923d6761fcb7f136c4c9dc773b7ae
Jean Tan Li <jeantl@outlook.com.br>
2021-04-01 15:48:21 -0300

    Aula 160 feita!

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -15,13 +16,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
       model: UserModel(),
-      child: MaterialApp(
-        title: 'Estrela Oriental',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            primaryColor: Color.fromARGB(255, 4, 125, 141)),
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
+      child: ScopedModelDescendant(
+        builder: (context, child, model) {
+          return ScopedModel<CartModel>(
+            model: CartModel(model),
+            child: MaterialApp(
+              title: 'Estrela Oriental',
+              theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                  primaryColor: Color.fromARGB(255, 4, 125, 141)),
+              debugShowCheckedModeBanner: false,
+              home: HomeScreen(),
+            ),
+          );
+        },
       ),
     );
   }
