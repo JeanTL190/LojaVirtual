048410ee3d3f1fac230ac3561a8f61d6e70210a7
Jean Tan Li <jeantl@outlook.com.br>
2021-04-04 12:28:01 -0300

    Aula 178 feita!

--- loja_virtual/lib/tabs/orders_tab.dart
+++ loja_virtual/lib/tabs/orders_tab.dart
@@ -22,8 +22,11 @@ class OrdersTab extends StatelessWidget {
             );
           } else {
             return ListView(
-              children:
-                  snapshot.data.docs.map((doc) => OrderTile(doc.id)).toList(),
+              children: snapshot.data.docs
+                  .map((doc) => OrderTile(doc.id))
+                  .toList()
+                  .reversed
+                  .toList(),
             );
           }
         },
