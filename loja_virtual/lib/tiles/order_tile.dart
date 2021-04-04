4f2f59405115869e799ca3ab3462a2f302f028c8
Jean Tan Li <jeantl@outlook.com.br>
2021-04-04 11:50:31 -0300

    Aula 177 feita!

--- loja_virtual/lib/tiles/order_tile.dart
+++ loja_virtual/lib/tiles/order_tile.dart
@@ -23,6 +23,7 @@ class OrderTile extends StatelessWidget {
                 child: CircularProgressIndicator(),
               );
             } else {
+              int status = snapshot.data["status"];
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
@@ -32,6 +33,30 @@ class OrderTile extends StatelessWidget {
                   ),
                   SizedBox(height: 4),
                   Text(_buildProductsText(snapshot.data)),
+                  SizedBox(height: 4),
+                  Text(
+                    "Status do pedido:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      _buildCircle("1", "Preparação", status, 1),
+                      Container(
+                        height: 1,
+                        width: 40,
+                        color: Colors.grey[500],
+                      ),
+                      _buildCircle("2", "Embalamento", status, 2),
+                      Container(
+                        height: 1,
+                        width: 40,
+                        color: Colors.grey[500],
+                      ),
+                      _buildCircle("3", "Entrega", status, 3),
+                    ],
+                  ),
                 ],
               );
             }
@@ -51,5 +76,49 @@ class OrderTile extends StatelessWidget {
     text += "Frete: ${snapshot.data()["shipPrice"].toStringAsFixed(2)}\n";
     text += "Total: R\$ ${snapshot.data()["totalPrice"].toStringAsFixed(2)}";
     return text;
+  }
+
+  Widget _buildCircle(
+      String title, String subtitle, int status, int thisStatus) {
+    Color backColor;
+    Widget child;
+
+    if (status < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      );
+    } else if (status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(
+        Icons.check,
+        color: Colors.white,
+      );
+    }
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subtitle),
+      ],
+    );
   }
 }
